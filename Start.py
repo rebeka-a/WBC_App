2b65769d8813ea412656fa9ce8d77ebc9eded946
Rebeka Ammann <rbkmmnn@gmail.com>
2025-03-25 21:30:08 +0100

    Swich Drive

--- Start.py
+++ Start.py
@@ -1,6 +1,19 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+from utils.data_manager import DataManager
+from utils.login_manager import LoginManager
+
+# Initialize the data manager
+data_manager = DataManager(fs_protocol='webdav', fs_root_folder="WBC_Data")  # switch drive
+
+
+# Load the data from the persistent storage into the session state
+data_manager.load_user_data(
+    session_state_key='data_df', 
+    file_name='data.csv', 
+    initial_value=pd.DataFrame(), 
+    parse_dates=['timestamp']
+)
 
 # Titel der App
 st.set_page_config(page_title="Blood Cell Counter", layout="wide")
